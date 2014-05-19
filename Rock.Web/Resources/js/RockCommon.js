520eccf281238d4857529075be5a2b625eaa7809
Carl <157147899@qq.com>
2014-05-19 20:25:23 +0800

    update

---
+++ Rock.Web/Resources/js/RockCommon.js
@@ -0,0 +1,41 @@
+﻿Date.prototype.format = function (format) {
+    var o = {
+        "M+": this.getMonth() + 1, //month 
+        "d+": this.getDate(), //day 
+        "h+": this.getHours(), //hour 
+        "m+": this.getMinutes(), //minute 
+        "s+": this.getSeconds(), //second 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter 
+        "S": this.getMilliseconds() //millisecond 
+    }
+
+    if (/(y+)/.test(format)) {
+        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(format)) {
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+        }
+    }
+    return format;
+};
+
+
+$.extend(
+{
+    //将unix时间格式转成ISO普通时间格式
+    getDateFromUnix: function (unixtime) {
+        var unixTimestamp = new Date(unixtime * 1000).format("yyyy-MM-dd hh:mm:ss");
+        //        commonTime = unixTimestamp.toLocaleDateString();
+        commonTime = unixTimestamp;
+        return commonTime;
+    },
+    getUnixDate: function (str) {
+        date = str.replace(/-/g, "/");
+        var date = new Date(str);
+        var humanDate = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()));
+        alert(humanDate.getTime() / 1000 - 8 * 60 * 60);     
+    }
+}
+);
--- Rock.Web/Resources/js/RockDataGrid.js
+++ Rock.Web/Resources/js/RockDataGrid.js
@@ -14,16 +14,19 @@ RockDataGridObj.prototype = {
         this.params.enabledSort = this.params.enabledSort || false;
         this.params.rownumbers = this.params.rownumbers || true;
         this.params.height = this.params.height || '95%';
-        this.params.width = this.params.width || '100%';
-        this.params.toolbar =
-        {
-            items:
-                    [
-                        { text: '增加', img: '/Resources/js/ligerUI/lib/ligerUI/skins/icons/add.gif' }
-                    ]
-        };
+        this.params.width = this.params.width || '98%';
+        this.params.fixedCellHeight = this.params.fixedCellHeight || false;
+        if (this.params.toolbar == undefined) {
+            this.params.toolbar =
+            {
+                items:
+                        [
+                            { text: '增加', img: '/Resources/js/ligerUI/lib/ligerUI/skins/icons/add.gif' }
+                        ]
+            };
+        }
 
-        if (this.params.defaultEdit) {
+        if (this.params.defaultEdit != undefined) {
             var editColumn = { display: '编辑', render: this.generateEditColumn };
             this.params.columns.push(editColumn);
             this.bindDefaultEditEvent();
@@ -33,52 +36,78 @@ RockDataGridObj.prototype = {
         this.params.grid = ligerGrid;
     },
     generateEditColumn: function (rowdata, index, value) {
+        var edithtml = '';
         var id = rowdata[localGridObj.params.pkName];
-        var edithtml = "<input type='button' value='编辑' data-Edit='true' data-ID='" + id + "'/>&nbsp;&nbsp;<input type='button' value='删除' data-Del='true' data-ID='" + id + "'/>";
+        switch (localGridObj.params.defaultEdit) {
+            case true:
+                edithtml = "<input type='button' value='编辑' data-Edit='true' data-ID='" + id + "'/><input type='button' value='删除' data-Del='true' data-ID='" + id + "'/>";
+                break;
+            case 'edit':
+                edithtml = "<input type='button' value='编辑' data-Edit='true' data-ID='" + id + "'/>";
+                break;
+            case 'del':
+                edithtml = "<input type='button' value='删除' data-Del='true' data-ID='" + id + "'/>";
+                break;
+            default:
+
+        }
         return edithtml;
     },
     bindDefaultEditEvent: function () {
-        var editUrl = this.params.editUrl;
-        var delUrl = this.params.delUrl;
-        $("input[data-Edit='true']").live("click", function () {
-            var id = $(this).attr('data-ID');
-            $.ligerDialog.open({ height: 500, width: 800, url: editUrl + id });
-        });
+        if (this.params.defaultEdit || this.params.defaultEdit == 'edit') {
+            var editUrl = this.params.editUrl;
+            $("input[data-Edit='true']").live("click", function () {
+                var id = $(this).attr('data-ID');
+                $.ligerDialog.open({ height: 500, width: 800, url: editUrl + id });
+            });
+        }
 
-        $("input[data-Del='true']").live("click", function () {
-            var id = $(this).attr('data-ID');
-            $.ligerDialog.confirm('确认要删除吗？', function (yes) {
-                if (yes) {
-                    $.ajax({
-                        type: "POST",
-                        url: delUrl + id,
-                        dataType: 'json',
-                        success: function (data) {
-                            if (data.Status == 0) {
-                                if (data.Msg != undefined) {
-                                    $.ligerDialog.success(data.Msg)
-                                }
-                                else {
-                                    $.ligerDialog.success('删除成功！')
+        if (this.params.defaultEdit || this.params.defaultEdit == 'del') {
+            var delUrl = this.params.delUrl;
+            $("input[data-Del='true']").live("click", function () {
+                var id = $(this).attr('data-ID');
+                $.ligerDialog.confirm('确认要删除吗？', function (yes) {
+                    if (yes) {
+                        $.ajax({
+                            type: "DELETE",
+                            url: delUrl + id,
+                            dataType: 'json',
+                            success: function (data) {
+                                if (data.Status == 'Success') {
+                                    if (data.Msg != undefined) {
+                                        $.ligerDialog.success(data.Msg)
+                                    }
+                                    else {
+                                        $.ligerDialog.success('删除成功！');
+                                    }
                                     localGridObj.params.grid.reload();
                                 }
-                            }
-                            else {
-                                if (data.Msg != undefined) {
-                                    $.ligerDialog.error(data.Msg)
-                                }
                                 else {
-                                    $.ligerDialog.error('删除失败！')
+                                    if (data.Msg != undefined) {
+                                        $.ligerDialog.error(data.Msg)
+                                    }
+                                    else {
+                                        $.ligerDialog.error('删除失败！')
+                                    }
                                 }
+                            },
+                            error: function () {
+                                $.ligerDialog.error('删除失败，未知错误。')
                             }
-                        },
-                        error: function () {
-                            $.ligerDialog.error('删除失败，未知错误。')
-                        }
-                    });
-                }
+                        });
+                    }
+                });
             });
-        });
+        }
+    },
+    getSelectedPK: function () {
+        var pks = [];
+        var pkname = this.params.pkName;
+        var selected = this.params.grid.getSelecteds();
+        for (var i = 0; i < selected.length; i++) {
+            pks.push(selected[i][pkname]);
+        }
+        return pks;
     }
 };
 
